25f71a937cf05c3632bf53c09f7dea5dfd05a099
dandanbang <daniel@berkeley.ischool.edu>
2018-04-17 14:21:56 -0700

    class exercise

---
+++ contracts/choice_voting.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.11;
+// We have to specify what version of compiler this code will compile with
+
+contract Voting {
+  /* mapping field below is equivalent to an associative array or hash.
+  The key of the mapping is candidate name stored as type bytes32 and value is
+  an unsigned integer to store the vote count
+  */
+
+  mapping (bytes32 => uint8) public votesReceived;
+
+  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
+  We will use an array of bytes32 instead to store the list of candidates
+  */
+
+  bytes32[] public choiceList;
+
+  /* This is the constructor which will be called once when you
+  deploy the contract to the blockchain. When we deploy the contract,
+  we will pass an array of candidates who will be contesting in the election
+  */
+  function Voting(bytes32[] choiceNames) {
+    choiceList = choiceNames;
+  }
+
+  // This function returns the total votes a candidate has received so far
+  function totalVotesFor(bytes32 choice) returns (uint8) {
+    if (validChoice(choice) == false) throw;
+    return votesReceived[choice];
+  }
+
+  // This function increments the vote count for the specified candidate. This
+  // is equivalent to casting a vote
+  function voteForChoice(bytes32 choice) {
+    if (validChoice(choice) == false) throw;
+    votesReceived[choice] += 1;
+  }
+
+  function validChoice(bytes32 choice) returns (bool) {
+    for(uint i = 0; i < choiceList.length; i++) {
+      if (choiceList[i] == choice) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
